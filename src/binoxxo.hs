61d3d1a1cc70a3c9b59286cbe2422bcc20e5cbe5
Pete Ryland <pdr@pdr.cx>
2014-10-31 14:58:25 +0100

    Rearrange functions, remove extra newline at end

--- src/binoxxo.hs
+++ src/binoxxo.hs
@@ -16,10 +16,10 @@ instance Show Cell where
              U -> "_"
 
 instance Show Row where
-  show row = (concat $ map show row) ++ "\n"
+  show row = concatMap show row ++ "\n"
 
 instance Show Grid where
-  show grid = concat $ map show grid
+  show grid = concatMap show grid
 
 instance Read Row where
   readsPrec _ "" = []
@@ -34,6 +34,9 @@ instance Read Row where
         | x == 'x'  = readsPrec' (row ++ [X]) xs
       readsPrec' row xs = [(row ++ replicate (10 - length row) U, xs)]
 
+readGrid :: IO Grid
+readGrid = sequence $ replicate 10 $ read <$> getLine
+
 type QRow = (Int, Int) -- (bit set of X/O, mask for unknowns)
 type QGrid = [QRow]
 
@@ -47,6 +50,9 @@ rowToQRow row = foldl' r2qr (0, 0) row
         O -> (val', mask' .|. 1)
         X -> (val' .|. 1, mask' .|. 1)
 
+gridToQGrid :: Grid -> QGrid
+gridToQGrid = map rowToQRow
+
 qRowToRow :: QRow -> Row
 qRowToRow (val, mask) = qr2r [] 1
   where
@@ -57,6 +63,12 @@ qRowToRow (val, mask) = qr2r [] 1
         _ -> case val .&. n of
           0 -> qr2r (O:row) (n `shiftL` 1)
           _ -> qr2r (X:row) (n `shiftL` 1)
+
+qGridToGrid :: QGrid -> Grid
+qGridToGrid = map qRowToRow
+
+transposeQGrid :: QGrid -> QGrid
+transposeQGrid = gridToQGrid . transpose . qGridToGrid
 
 validQRows :: [Int]
 validQRows = [155,171,173,179,181,182,203,205,211,213,214,217,218,299,301,307,309,310,331,333,339,341,342,345,346,357,358,361,362,364,403,405,406,409,410,421,422,425,426,428,434,436,587,589,595,597,598,601,602,613,614,617,618,620,659,661,662,665,666,677,678,681,682,684,690,692,713,714,716,722,724,805,806,809,810,812,818,820,841,842,844,850,852,868] -- length 10
@@ -69,18 +81,6 @@ getValidQRow (val, mask) =
              mask' = (foldl1' (.&.) vals) .|. (foldl' (\x -> (x .&.) . complement) 1023 vals)
              val' = mask' .&. (head vals)
          in (val', mask')
-
-gridToQGrid :: Grid -> QGrid
-gridToQGrid = map rowToQRow
-
-qGridToGrid :: QGrid -> Grid
-qGridToGrid = map qRowToRow
-
-readGrid :: IO Grid
-readGrid = sequence $ replicate 10 $ read <$> getLine
-
-transposeQGrid :: QGrid -> QGrid
-transposeQGrid = gridToQGrid . transpose . qGridToGrid
 
 tryRow :: QRow -> (QRow, Bool) -- solve as much as possible, True if changed
 tryRow (val, mask) =
@@ -110,8 +110,7 @@ keepTryingBoth grid = tryBoth $ fst $ tryGrid grid
 solve :: Grid -> Grid
 solve = qGridToGrid . keepTryingBoth . gridToQGrid
 
-
 main :: IO ()
 main = do grid <- readGrid
           putStrLn $ show grid
-          putStrLn $ show $ solve grid
+          putStr $ show $ solve grid
