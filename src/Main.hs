17e7534876852bc4348e86a1776e7da87ac2b291
Pete Ryland <pdr@pdr.cx>
2014-11-07 05:59:14 +0100

    Use a monad for the try functions to propagate whether something has changed or not

--- src/Main.hs
+++ src/Main.hs
@@ -5,46 +5,73 @@ import ValidQRows(validQRows)
 import Data.Bits(shiftL, (.&.), (.|.), complement)
 import Data.List(foldl', foldl1', (\\))
 import Control.Applicative((<$>))
-import Control.Monad.State(State, evalState, get, put)
+import Control.Monad(liftM, (>=>))
+import Control.Monad.Trans(lift)
+import Control.Monad.State(StateT, evalStateT, get, put)
 import Text.Parsec(parse)
+
+data TryResult a = TR a Bool -- (the result value, has anything changed?)
+
+instance Functor TryResult where
+  fmap f (TR x y) =
+    TR (f x) y
+
+instance Monad TryResult where
+  (TR x changed) >>= f =
+    let TR y changed' = f x
+    in TR y (changed || changed')
+  return x =
+    TR x False
+
+setChangedFlag :: Bool -> TryResult a
+setChangedFlag = TR undefined
 
 fullMask :: Int -> Int
 fullMask len =
   (1 `shiftL` len) - 1
 
-tryRow :: Int -> QRow -> State [Int] (QRow, Bool) -- solve as much as possible, True if changed
+isDone :: Int -> Int -> Bool
+isDone len mask =
+  mask == fullMask len
+
+tryRow :: Int -> QRow -> StateT [Int] TryResult QRow -- try to solve as much as possible
 tryRow len (val, mask) = do
   possibleQRows <- get
-  let vals = filter (\x -> x .&. mask == val) possibleQRows
-      mask' = (foldl1' (.&.) vals) .|. (foldl' (\x -> (x .&.) . complement) (fullMask len) vals)
-      val' = mask' .&. (head vals)
-  if mask == fullMask len || vals == [] then
-    return ((val, mask), False)
+  let validRows = filter (\x -> x .&. mask == val) possibleQRows
+      eachIs1 = foldl1' (.&.) validRows
+      eachIs0 = foldl' (.&.) (fullMask len) (map complement validRows)
+      mask' =  eachIs1 .|. eachIs0
+      val' = mask' .&. (head validRows)
+  if isDone len mask || validRows == [] then
+    return (val, mask)
   else do
     -- if you don't want the uniqueness rule, remove the following four lines
-    put $ if mask' == fullMask len then   -- alternatively: if tail vals == []
-      possibleQRows \\ vals
+    put $ if isDone len mask' then
+      possibleQRows \\ validRows
     else
       possibleQRows
-    return ((val', mask'), mask /= mask')
+    lift . setChangedFlag $ mask /= mask'
+    return (val', mask')
 
-tryGrid :: QGrid -> (QGrid, Bool) -- solve as much as possible, True if changed
-tryGrid (len, grid) =
-  let completeRows = foldl (\x (val, mask) -> if mask == fullMask len then val:x else x) [] grid
-      possibleQRows = validQRows len \\ completeRows
-      result = evalState (mapM (tryRow len) grid) possibleQRows
-  in ((len, map fst result), or $ map snd result)
+tryGridRows :: QGrid -> TryResult QGrid -- try to solve as much as possible
+tryGridRows (len, grid) = do
+  let completeRows = map fst $ filter (isDone len . snd) grid
+      possibleQRows = validQRows len \\ completeRows -- TODO: should use Data.Set
+  r <- evalStateT (mapM (tryRow len) grid) possibleQRows
+  return (len, r)
 
-tryGridT :: QGrid -> (QGrid, Bool) -- as above, but work on cols instead
-tryGridT grid =
-  let (grid', changed) = tryGrid $ transposeQGrid grid
-  in (transposeQGrid grid', changed)
+tryGridCols :: QGrid -> TryResult QGrid -- as above, but work on cols instead
+tryGridCols =
+  liftM transposeQGrid . tryGridRows . transposeQGrid
 
-keepTryingBoth :: QGrid -> QGrid -- first try rows, then if something changed, try cols, ...
-keepTryingBoth grid =
-  let (grid', changed') = tryGrid grid
-      (grid'', changed'') = tryGridT grid'
-  in if changed' || changed'' then keepTryingBoth grid'' else grid''
+keepTryingBoth :: QGrid -> QGrid
+keepTryingBoth =
+  mapIf keepTryingBoth . (tryGridRows >=> tryGridCols)
+  where
+    mapIf f (TR x c) =
+      case c of
+        True -> f x
+        False -> x
 
 solve :: Grid -> Grid
 solve =
